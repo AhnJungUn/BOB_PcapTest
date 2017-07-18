a9657a906a8b544e20c38269c5e02a024ad5a85a
root <root@localhost.localdomain>
2017-07-18 03:43:58 -0400

    change the code through the class

--- pcap_test.c
+++ pcap_test.c
@@ -8,92 +8,107 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
+void packet_analysis(const u_char *packet)
 {
-    struct libnet_ethernet_hdr *ethhdr;
-    struct libnet_ipv4_hdr *iphdr;
-    struct libnet_tcp_hdr *tcphdr;
-   
-    unsigned short ether_type;
-    unsigned short ip_type;    
-    int chcnt =0;
-    int length=pkthdr->len;
+	struct libnet_ethernet_hdr *ethhdr;
+	struct libnet_ipv4_hdr *iphdr;
+	struct libnet_tcp_hdr *tcphdr;
 
-    ethhdr = (struct libnet_ethernet_hdr *)packet;
-    packet += sizeof(struct libnet_ethernet_hdr);
+	unsigned short ether_type;
+	unsigned short ip_type; 
+	char buf[20];   
+	int chcnt =0;
+	int length;
 
-    printf("< Ethernet header >\n\n");
-    printf("ethernet Src Mac  : %s\n",(char*)ether_ntoa(ethhdr->ether_shost));
-    printf("ethernet Dst Mac  : %s\n\n",(char*)ether_ntoa(ethhdr->ether_dhost));	
-    
-    ether_type = ntohs(ethhdr->ether_type); 
+	ethhdr = (struct libnet_ethernet_hdr *)packet;
+	packet += sizeof(struct libnet_ethernet_hdr);
 
-    if (ether_type == ETHERTYPE_IP) 
-    {
-        iphdr = (struct libnet_ipv4_hdr *)packet;
-	printf("< IP Packet >\n\n");
-        printf("Src IP  : %s\n", inet_ntoa(iphdr->ip_src)); 
-        printf("Dst IP  : %s\n\n", inet_ntoa(iphdr->ip_dst));
-    }
-    else
-    {
-	printf("< No IP_Packet > \n\n");
-	printf("-----------------------------------------------\n\n");
-	return;
-    }
+	printf("< Ethernet header >\n\n");
+	printf("ethernet Src Mac  : %s\n",(char*)ether_ntoa(ethhdr->ether_shost));
+	printf("ethernet Dst Mac  : %s\n\n",(char*)ether_ntoa(ethhdr->ether_dhost));	
 
-    ip_type=iphdr->ip_p;
-	
-    if(ip_type == 0x06)
-    {
-	packet += iphdr->ip_hl * 4; 
-	tcphdr = (struct libnet_tcp_hdr *)(packet); 
-	printf("< TCP Packet >\n\n");
-        printf("Src Port : %d\n" , ntohs(tcphdr->th_sport));
-        printf("Dst Port : %d\n\n" , ntohs(tcphdr->th_dport));
-	    
-	packet += tcphdr->th_off * 4;
-	length = length - sizeof(struct libnet_ethernet_hdr) - iphdr->ip_hl - tcphdr->th_off;   
-	
-	printf("< DATA >\n\n");    
-	while(length--)
-        {
-             printf("%02x", *(packet++)); 
-             if ((++chcnt % 16) == 0) 
-                 printf("\n");
+	ether_type = ntohs(ethhdr->ether_type); 
+
+	if (ether_type == ETHERTYPE_IP) 
+	{	
+		length = iphdr->ip_len;
+		iphdr = (struct libnet_ipv4_hdr *)packet;
+		printf("< IP Packet >\n\n");
+		inet_ntop(AF_INET, &iphdr->ip_src, buf, sizeof(buf));
+		printf("Src IP  : %s\n", buf);
+		inet_ntop(AF_INET, &iphdr->ip_dst, buf, sizeof(buf)); 
+		printf("Dst IP  : %s\n\n", buf);
 	}
-	printf("\n\n");
-     }
-     else
-     {
-     	printf("< No TCP_Packet >\n\n");
-	printf("-----------------------------------------------\n\n");
-	return;
-     }	
+	else
+	{
+		printf("< No IP_Packet > \n\n");
+		printf("-----------------------------------------------\n\n");
+		return;
+	}
+
+	ip_type = iphdr->ip_p;
+
+	if(ip_type == IPPROTO_TCP)
+	{
+		packet += iphdr->ip_hl * 4; 
+		tcphdr = (struct libnet_tcp_hdr *)(packet); 
+		printf("< TCP Packet >\n\n");
+		printf("Src Port : %d\n" , ntohs(tcphdr->th_sport));
+		printf("Dst Port : %d\n\n" , ntohs(tcphdr->th_dport));
+
+		packet += tcphdr->th_off * 4;
+		length = length - iphdr->ip_hl*4 - tcphdr->th_off*4;   
+
+		printf("< DATA >\n\n");    
+		while(length--)
+		{
+			printf("%02x", *(packet++)); 
+			if ((++chcnt % 16) == 0) 
+				printf("\n");
+		}
+		printf("\n\n");
+		printf("-----------------------------------------------\n\n");
+	}
+	else
+	{
+		printf("< No TCP_Packet >\n\n");
+		printf("-----------------------------------------------\n\n");
+		return;
+	}	
 
 }
 
 int main(int argc, char **argv)
 {
-    char *dev;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *pcd;  // packet capture descriptor
-    
-    dev = pcap_lookupdev(errbuf);
-    
-    if (dev == NULL)
-    {
-        printf("%s\n", errbuf);
-        exit(1);
-    }
-  
-    pcd = pcap_open_live(dev, BUFSIZ, 1/*PROMISCUOUS*/, -1, errbuf);
-    
-    if (pcd == NULL)
-    {
-        printf("%s\n", errbuf);
-        exit(1);
-    }    
+	char *dev;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *pcd;  // packet capture descriptor
+	struct pcap_pkthdr *header;
+	const u_char *packet;
+	int res = 0;
+		
+	dev = pcap_lookupdev(errbuf);
 
-    pcap_loop(pcd, 0, callback, NULL);
+	if (dev == NULL)
+	{
+		printf("%s\n", errbuf);
+		exit(1);
+	}
+
+	pcd = pcap_open_live(dev, BUFSIZ, 1/*PROMISCUOUS*/, -1, errbuf);
+
+	if (pcd == NULL)
+	{
+		printf("%s\n", errbuf);
+		exit(1);
+	}    
+	while(1)
+	{
+
+		res = pcap_next_ex(pcd, &header, &packet);
+		if(res > 0)
+			packet_analysis(packet);
+		else
+			continue;
+	}
 }
